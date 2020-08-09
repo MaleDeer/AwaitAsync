fc6dd4004b1453d9c36b03361cfaa2e414687aa5
马俊程 <upgrader@qq.com>
2020-08-09 21:28:35 +0800

    添加项目文件。

---
+++ AwaitAsync/Desktop.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace AwaitAsync
+{
+    partial class Desktop
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAsync = new System.Windows.Forms.Button();
+            this.btnAsync2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnAsync
+            // 
+            this.btnAsync.Location = new System.Drawing.Point(13, 13);
+            this.btnAsync.Name = "btnAsync";
+            this.btnAsync.Size = new System.Drawing.Size(235, 28);
+            this.btnAsync.TabIndex = 0;
+            this.btnAsync.Text = "Async";
+            this.btnAsync.UseVisualStyleBackColor = true;
+            this.btnAsync.Click += new System.EventHandler(this.btnAsync_Click);
+            // 
+            // btnAsync2
+            // 
+            this.btnAsync2.Location = new System.Drawing.Point(13, 57);
+            this.btnAsync2.Name = "btnAsync2";
+            this.btnAsync2.Size = new System.Drawing.Size(235, 28);
+            this.btnAsync2.TabIndex = 0;
+            this.btnAsync2.Text = "Async2";
+            this.btnAsync2.UseVisualStyleBackColor = true;
+            this.btnAsync2.Click += new System.EventHandler(this.btnAsync2_Click);
+            // 
+            // Desktop
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(260, 100);
+            this.Controls.Add(this.btnAsync2);
+            this.Controls.Add(this.btnAsync);
+            this.Name = "Desktop";
+            this.Text = "Desktop";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Button btnAsync2;
+        private System.Windows.Forms.Button btnAsync;
+    }
+}
---
+++ AwaitAsync/Desktop.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AwaitAsync
+{
+    public partial class Desktop : Form
+    {
+        public Desktop()
+        {
+            InitializeComponent();
+        }
+
+        public static string GetThreadId()
+        {
+            return Thread.CurrentThread.ManagedThreadId.ToString("000");
+        }
+
+        public static void TaskRun()
+        {
+            Console.WriteLine($"======== ThreadId:{GetThreadId()} Start.");
+            Thread.Sleep(3000);
+            Console.WriteLine($"======== ThreadId:{GetThreadId()} End.");
+        }
+
+        #region async/await
+
+        private void btnAsync_Click(object sender, EventArgs e)
+        {
+            Task task = NoReturnTask();
+        }
+
+        private async Task NoReturnTask()
+        {
+            Console.WriteLine($"==== ThreadId:{GetThreadId()} Start.");
+            await Task.Run(() => TaskRun());
+            Console.WriteLine($"==== ThreadId:{GetThreadId()} End.");
+        }
+
+        #endregion async/await
+
+        #region async/await 2
+
+        private void btnAsync2_Click(object sender, EventArgs e)
+        {
+            var stateMachine = new AsyncStateMachine();
+            stateMachine.builder = AsyncTaskMethodBuilder.Create();
+            stateMachine.state = -1;
+            stateMachine.builder.Start(ref stateMachine);
+            Task task = stateMachine.builder.Task;
+        }
+
+        private class AsyncStateMachine : IAsyncStateMachine
+        {
+            public int state;
+            public AsyncTaskMethodBuilder builder;
+            private TaskAwaiter taskAwaiter;
+
+            public void MoveNext()
+            {
+                int num = state;
+                try
+                {
+                    TaskAwaiter awaiter;
+                    if (num != 0)
+                    {
+                        Console.WriteLine("==== ThreadId:" + GetThreadId() + " Start.");
+                        awaiter = Task.Run(() => TaskRun()).GetAwaiter();
+                        if (!awaiter.IsCompleted)
+                        {
+                            num = state = 0;
+                            taskAwaiter = awaiter;
+                            AsyncStateMachine stateMachine = this;
+                            builder.AwaitUnsafeOnCompleted(ref awaiter, ref stateMachine);
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        awaiter = taskAwaiter;
+                        taskAwaiter = default;
+                        num = state = -1;
+                    }
+                    awaiter.GetResult();
+                    Console.WriteLine($"==== ThreadId:{GetThreadId()} End.");
+                }
+                catch (Exception ex)
+                {
+                    state = -2;
+                    builder.SetException(ex);
+                    return;
+                }
+
+                state = -2;
+                builder.SetResult();
+            }
+
+            void IAsyncStateMachine.MoveNext()
+            {
+                MoveNext();
+            }
+
+            public void SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+            }
+
+            void IAsyncStateMachine.SetStateMachine(IAsyncStateMachine stateMachine)
+            {
+                SetStateMachine(stateMachine);
+            }
+        }
+
+        #endregion async/await 2
+    }
+}
